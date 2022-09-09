144887475c2448b28b7aefb706909d6bd3cedf8b
Sourfruit <hello@def-not-hacking-the.net>
2022-09-08 21:26:52 -0400

    ihateerrors.com

--- pages/500.js
+++ pages/500.js
@@ -1,4 +1,3 @@
-
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
@@ -10,25 +9,25 @@ import TopBar from '../components/homie/topbar'
 import Footer from '../components/footer'
 import * as Icon from "phosphor-react";
 
-export default function NotFound() {
+export default function ServerError() {
     return (
         <div className={styles.container}>
             <Head>
-                <title>404 | Takeout</title>
+                <title>500 | Takeout</title>
                 <meta name="viewport" content="width=device-width, initial-scale=0.7" />
-                <meta name="description" content="404 | A simple, affordable, developer-oriented email service." />
+                <meta name="description" content="500 | A simple, affordable, developer-oriented email service." />
                 <meta name="keywords" content="Takeout, email, nodemailer, email for node, webmail, developer mail, mail in js, oO, [], SO, object Object, [object Object] Stack Overflow, Stack, Programmer, Forum, Sourfruit, s0urfruit, sour, fruit, StackOverlow sourfruit, Google Analytics, privacy, analytics, TrackerKit, sourfruit analytics, anal, security, privacy first, developer analytics, easy" />
                 <meta name="author" content="Sourfruit" />
                 <meta property="og:url" content="https://takeout.bysourfruit.com" />
 
-                <meta property="og:title" content="404 | Takeout" />
-                <meta property="og:description" content="404 | A simple, affordable, developer-oriented email service." />
+                <meta property="og:title" content="500 | Takeout" />
+                <meta property="og:description" content="500 | A simple, affordable, developer-oriented email service." />
                 <meta property="og:type" content="website" />
                 <meta property="og:image" content="https://i.ibb.co/28RgWJs/takeout.png" />
                 <meta property="og:locale" content="en_US" />
                 <meta name="twitter:card" content="summary" />
-                <meta name="twitter:title" content="404 | Takeout" />
-                <meta name="twitter:description" content="404 | A simple, affordable, developer-oriented email service." />
+                <meta name="twitter:title" content="500 | Takeout" />
+                <meta name="twitter:description" content="500 | A simple, affordable, developer-oriented email service." />
                 <meta name="twitter:image" content="https://i.ibb.co/28RgWJs/takeout.png" />
                 <meta name="twitter:image:alt" content="Takeout" />
 
@@ -42,35 +41,19 @@ export default function NotFound() {
             <TopBar />
             <div className={styles.sections}>
                 <section className={styles.loginSecLeft}>
-                    <img src="https://i.ibb.co/fCVC3CW/404.png" width='300px' />
+                    <img src="https://i.ibb.co/wRPz26d/undraw-server-down-s-4-lk.png" width='300px' />
                 </section>
 
                 <section className={styles.loginSecRight}>
-                    
                     <div className={styles.loginRightText}>
                         <h1 className={styles.homeHook} style={{ marginBottom: '30px' }}>
-                            We couldn't find what <b>you're looking for.</b>
+                            Some extremely strange <b>500 error appeared.</b>
                         </h1>
                         <a href='/' className={styles.blueButton} style={{ verticalAlign: 'middle' }}>Take me home <Icon.CaretRight className={styles.buttIcon} style={{ fontSize: '1.1rem' }} weight='bold' /> </a>
                     </div>
-
                 </section>
             </div>
             <Footer/>
         </div>
     )
 }
-/* 
-        const faceArr = [
-            '(;-;)',
-            '(^-^*)',
-            '\\(^Д^)/',
-            '(·_·)',
-            '\\(o_o)/',
-            '(=\'X\'=)',
-            '(≥o≤)',
-        ];
-        const UwUFace = document.getElementById("defined");
-        const chosenface = faceArr[Math.floor(Math.random() * faceArr.length)];
-        UwUFace.innerText = chosenface;
-*/
